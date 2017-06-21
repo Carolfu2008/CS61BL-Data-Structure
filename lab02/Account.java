5fbf6f7a0a690572333c0b657698274d2dceac59
lifesaver0129 <huyuxing413@vip.qq.com>
2017-06-20 21:40:19 -0700

    test04

--- lab02/Account.java
+++ lab02/Account.java
@@ -13,7 +13,7 @@ public class Account {
 	/** Initialize an account with the given BALANCE. */
 	public Account(int balance) {
 		this.balance = balance;
-		this.parentAccount.balance = 0;
+		this.parentAccount= null;
 	}
 
 	public Account(int bal1, Account bal2) {
@@ -46,7 +46,7 @@ public class Account {
 		if (amount < 0) {
 			System.out.println("Cannot withdraw negative amount.");
 			return false;
-		} else if (this.balance < amount && this.balance + this.parentAccount.balance < amount) {
+		} else if (this.balance < amount && this.balance + this.parentAccount.balance < amount&&this.) {
 			System.out.println("Insufficient funds");
 			return false;
 		} else if (this.balance < amount && this.balance + this.parentAccount.balance < amount) {
