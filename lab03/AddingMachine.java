ae681060fd4403686810ca9b1cbf9e5942039129
Daniel Nguyen <danielnguyen880@yahoo.com>
2017-06-21 21:46:07 -0700

    release lab03

---
+++ lab03/AddingMachine.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class AddingMachine {
+
+	public static void main (String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		boolean isPreviousZero = false;
+		int total = 0;
+		int subtotal = 0;
+		int input;
+		int last = 1;
+		int MAXIMUM_NUMBER_OF_INPUTS = 100;
+    // TODO Add code here and elsewhere to complete AddingMachine
+		while (true) {
+			input = scanner.nextInt();
+			if (input == 0) {
+				if (isPreviousZero) {
+					System.out.println("total " + total);
+					return;
+				} else {
+					System.out.println("subtotal " + subtotal);
+					total += subtotal;
+					subtotal = 0;
+					isPreviousZero = true;
+				}
+			}
+			subtotal += input;
+			if (input != 0) {
+				isPreviousZero = false;
+			}
+		   
+		}
+	}
+
+}
