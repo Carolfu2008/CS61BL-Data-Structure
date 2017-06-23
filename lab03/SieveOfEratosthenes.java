0d252abbf1c03094ae87b02ebf55680b74523201
lifesaver0129 <huyuxing413@vip.qq.com>
2017-06-22 18:40:45 -0700

    try7

--- lab03/SieveOfEratosthenes.java
+++ lab03/SieveOfEratosthenes.java
@@ -3,6 +3,7 @@ public class SieveOfEratosthenes {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("You need to enter an argument!");
+            return;
         }
 
         int upperBound = Integer.parseInt(args[0]);
@@ -17,8 +18,6 @@ public class SieveOfEratosthenes {
                     isNotPrime[j]=true;
             }
         }
-
-        
         for (int i = 2; i < upperBound; i++) {
             if (!isNotPrime[i]) {
                 System.out.println(i + " is a prime number.");
